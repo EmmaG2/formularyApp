b2f627df511aedb3df0bc38a00b3b525144c93de
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-01-20 23:21:13 -0600

    basic template formulary

--- src/app/template/pages/basicos/basicos.component.ts
+++ src/app/template/pages/basicos/basicos.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  @ViewChild('miFormulario', { static: true }) miFormulario!: NgForm
 
-  ngOnInit(): void {
+  initForm = {
+    producto: 'RTX',
+    precio: 10,
+    existencias: 10
   }
 
-  guardar( miFormulario: NgForm ) {
-    console.log( miFormulario.value );
-
+  nombreVaido(): boolean {
+    return (
+      this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched
+    )
   }
 
+  precioValido() {
+    return (
+      this.miFormulario?.controls['precio']?.value < 100 &&
+      this.miFormulario?.controls['precio']?.touched
+    )
+  }
+
+  guardar() {
+    this.miFormulario.resetForm({
+      producto: 'No hay nada...',
+      precio: 0,
+      existencias: 0, // Este es un comentario
+    })
+  }
 }
