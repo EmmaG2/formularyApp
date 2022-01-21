89c0e9ca25fae1b748f9da1b3ffdd55712cfe019
Emmanuel Granados <88518706+EmmanuelRdezG2@users.noreply.github.com>
2022-01-20 23:22:49 -0600

    lazy load implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
   {
     path: 'reactive',
-    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule)
+    loadChildren: () =>
+      import('./reactive/reactive.module').then((m) => m.ReactiveModule),
   },
   {
     path: 'template',
-    loadChildren: () => import('./template/template.module').then( m => m.TemplateModule)
+    loadChildren: () =>
+      import('./template/template.module').then((m) => m.TemplateModule),
   },
   {
     path: '**',
-    redirectTo: 'template'
-  }
-];
+    redirectTo: 'template',
+  },
+]
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
